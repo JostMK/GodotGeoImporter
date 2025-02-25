a9932a9116ffe0c2505fa254a3a38492ab05ef71
JostMK <jost.klink@gmail.com>
2025-02-25 19:21:40 +0100

    Minor corrections and reformatting
    Removes macos binaries for now
    Runs formatter over all files
    Changes project name in CMakePresets.json

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -6,24 +6,33 @@
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+void initialize_example_module(ModuleInitializationLevel p_level)
+{
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
         return;
     }
 
     GDREGISTER_CLASS(GDExample);
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+void uninitialize_example_module(ModuleInitializationLevel p_level)
+{
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
         return;
     }
 }
 
-extern "C" {
+extern "C"
+{
     // Initialization.
-    GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-        godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+    GDExtensionBool GDE_EXPORT example_library_init(
+        GDExtensionInterfaceGetProcAddress p_get_proc_address,
+        const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
+    {
+        godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library,
+                                                       r_initialization);
 
         init_obj.register_initializer(initialize_example_module);
         init_obj.register_terminator(uninitialize_example_module);
